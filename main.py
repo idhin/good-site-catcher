54b783abade1264eb915991096f844fe9f3fadd1
Khulafaur Rasyidin <khulafaur.rasyidin@transtrack.id>
2023-10-19 13:42:14 +0700

    extract files

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
-
+import logging
+import sys
+import datetime
 import certstream
-import requests
-import os
-from datetime import datetime
-import configparser
+import tldextract
 
-def read_config():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config['Config']
+def print_callback(message, context):
+    logging.debug("Message -> {}".format(message))
 
-def save_website(domain):
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    folder_path = 'results'
-    file_path = f'{folder_path}/{timestamp}.txt'
+    if message['message_type'] == "heartbeat":
+        return
 
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    with open(file_path, 'w') as file:
-        file.write(domain)
-
-def certstream_callback(message, context):
     if message['message_type'] == "certificate_update":
         all_domains = message['data']['leaf_cert']['all_domains']
-        cert_validity = message['data']['leaf_cert']['validity']['end']
-        score = message['data']['leaf_cert']['validation']['score']
 
-        if score >= int(config['score_threshold']):
-            for domain in all_domains:
-                save_website(domain)
+        if len(all_domains) == 0:
+            domain = "NULL"
+        else:
+            domain = all_domains[0]
 
-config = read_config()
+        # Mengekstrak tld dari domain
+        ext = tldextract.extract(domain)
+        tld = f"{ext.domain}.{ext.suffix}"
 
-if not os.path.exists('results'):
-    os.makedirs('results')
+        # Menyimpan URL yang valid ke dalam results.txt
+        with open('results.txt', 'a') as f:
+            f.write(tld + '\n')
 
-certstream.listen_for_events(certstream_callback)
+        sys.stdout.write(u"[{}] {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), domain, ", ".join(message['data']['leaf_cert']['all_domains'][1:])))
+        sys.stdout.flush()
 
-certstream.run_for(int(config['run_duration_minutes']) * 60)
+logging.basicConfig(format='[%(levelname)s:%(name)s] %(asctime)s - %(message)s', level=logging.INFO)
+
+certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
