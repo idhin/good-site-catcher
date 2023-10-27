1acc068b84296f127bbb852e2d4f0ff07e584f63
Khulafaur Rasyidin <khulafaur.rasyidin@transtrack.id>
2023-10-27 12:08:35 +0700

    add blacklist word

--- cert/run.sh
+++ cert/run.sh
@@ -1,2 +1,11 @@
-#jangan lupa di chmod +x dulu validate domains nya
-certstream --json | jq -r '.data.leaf_cert.all_domains[]' | python3 validate_domains.py | tee -a output.txt
+# Periksa sertifikat pada aliran sertifikat
+certstream --json | jq -r '.data.leaf_cert.all_domains[]' | \
+
+# Validasi domain menggunakan skrip Python
+python3 validate_domains.py | \
+
+# Filter hasil dengan blacklist
+grep -v -f blacklist.txt | \
+
+# Simpan hasil ke output.txt
+tee -a output.txt
